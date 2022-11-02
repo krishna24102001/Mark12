01f666fa76708d3602292b36bac62d9dd3269d9b
krishna24102001 <krishnargite1@gmail.com>
2022-11-02 14:49:10 +0530

    Port links added

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,19 +4,19 @@ export default function Footer() {
   return (
     <footer>
     <ul>
-        <li>
-            <a href="https://www.linkedin.com/in/ajinkya-jadhav-760997196/" target="_blank">Linked in</a>
-        </li>
-        <li>
-            <a href="https://twitter.com/AjinkyaDineshJ1" target="_blank">Twitter</a>
-        </li>
-        <li>
-            <a href="https://github.com/ajinkya6373" target="_blank">GitHub</a>
-        </li>
-        <li>
-            <a href="https://ajinkya-portfolio.netlify.app/" target="_blank">Portfolio</a>
-        </li>
-    </ul>
+            <li>
+                <a href="https://www.linkedin.com/in/krishna-gite-00b418234/" target="_blank">Linked in</a>
+            </li>
+            <li>
+                <a href="https://twitter.com/KrishnaRGite" target="_blank">Twitter</a>
+            </li>
+            <li>
+                <a href="https://github.com/krishna24102001" target="_blank">GitHub</a>
+            </li>
+            <li>
+                <a href="https://portfoliokrishnagite.netlify.app/" target="_blank">Portfolio</a>
+            </li>
+        </ul>
    
 </footer>
 
